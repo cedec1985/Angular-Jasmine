6de786c5d77a1fc9434f9c648bbc859d3f78ccfd
Cédric De Craim <cedricdecraim@msn.com>
2025-07-31 13:50:41 +0200

    ok

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,11 +1,12 @@
-import { TestBed, async, ComponentFixture } from '@angular/core/testing';
+import { TestBed,ComponentFixture } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+import {APP_BASE_HREF}    from '@angular/common';
 
 describe('AppComponent', () => {
   let component: AppComponent;
   let fixture: ComponentFixture<AppComponent>;
 
-beforeEach(async(() => {
+beforeEach(() => {
     TestBed.configureTestingModule({
       imports: [
 
@@ -13,12 +14,15 @@ beforeEach(async(() => {
       declarations: [
         AppComponent
       ],
+      providers: [
+    {provide: APP_BASE_HREF, useValue: '/'}
+  ]
       // Prefer to declare child components unless things are too complex, else use schema
       // schemas: [NO_ERRORS_SCHEMA]
     }).compileComponents();
     fixture = TestBed.createComponent(AppComponent);
     component = fixture.componentInstance;
-  }));
+  });
 
   it('should create the app', () => {
     const fixture = TestBed.createComponent(AppComponent);
