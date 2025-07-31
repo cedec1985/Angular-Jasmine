de072a96b07ffd8e63a3d1b0a3fc7374178377e3
Cédric De Craim <cedricdecraim@msn.com>
2025-07-31 13:43:15 +0200

    ok

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,1 +1,28 @@
+import { TestBed, async, ComponentFixture } from '@angular/core/testing';
+import { AppComponent } from './app.component';
 
+describe('AppComponent', () => {
+  let component: AppComponent;
+  let fixture: ComponentFixture<AppComponent>;
+
+beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      imports: [
+
+      ],
+      declarations: [
+        AppComponent
+      ],
+      // Prefer to declare child components unless things are too complex, else use schema
+      // schemas: [NO_ERRORS_SCHEMA]
+    }).compileComponents();
+    fixture = TestBed.createComponent(AppComponent);
+    component = fixture.componentInstance;
+  }));
+
+  it('should create the app', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.debugElement.componentInstance;
+    expect(app).toBeTruthy();
+  });
+});
