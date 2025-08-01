028f52b3fa6b987dc155890384a6e7fba20b50b2
Cédric De Craim <cedricdecraim@msn.com>
2025-08-01 11:13:55 +0200

    plugins

--- src/spec/support/jasmine.mjs
+++ src/spec/support/jasmine.mjs
@@ -5,6 +5,10 @@ export default {
     "app/*.ts",
     "app/*.spec.?(m)js",
     "app/*.spec.ts",
+    "dist/out-tsc/**/*.?(m)js",
+    "dist/out-tsc/**/*.ts",
+    "dist/out-tsc/**/*.spec.?(m)js",
+    "dist/out-tsc/**/*.spec.ts",
   ],
   helpers: [
     "helpers/**/*.?(m)js"
