99c72e9232c36ae93550b01009f61873a7f1030b
Cédric De Craim <cedricdecraim@msn.com>
2025-08-05 12:26:23 +0200

    routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CounterComponent } from './components/counter/counter-component';
+import { Homecomponent } from './components/home/homecomponent/homecomponent';
 
 export const routes: Routes = [
-  { path: '', pathMatch: 'full', component: HomeComponent },
+  { path: '', pathMatch: 'full', component: Homecomponent },
   { path: 'counter-component', component: CounterComponent }
 ];
 @NgModule({
