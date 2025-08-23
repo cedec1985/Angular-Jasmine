b923a2de971307cab4b9f329b0ec27bd1c06a139
Cédric De Craim <69531112+cedec1985@users.noreply.github.com>
2025-08-23 22:35:24 +0200

    Update page-not-found.spec.ts

--- src/app/components/page-not-found/page-not-found.spec.ts
+++ src/app/components/page-not-found/page-not-found.spec.ts
@@ -1,5 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-import { PageNotFound } from './page-not-found.js';
+import { PageNotFound } from './page-not-found';
 
 describe('PageNotFound', () => {
   let component: PageNotFound;
