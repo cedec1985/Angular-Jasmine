b02025a6c9008b606fc0e98452549fa64c8df04b
Cédric De Craim <cedricdecraim@msn.com>
2025-08-23 17:03:35 +0200

    supprimer le dossier build

--- src/build/directives/attribute/attribut.directive.spec.js
+++
@@ -1,8 +0,0 @@
-import { Attribut } from './attribut.directive.js';
-describe('Attribut', () => {
-    it('should create an instance', () => {
-        const directive = new Attribut();
-        expect(directive).toBeTruthy();
-    });
-});
-//# sourceMappingURL=attribut.directive.spec.js.map
