2246eae634fefa3cf845b2c19f673c253dabdf71
Cédric De Craim <cedricdecraim@msn.com>
2025-08-26 10:43:14 +0200

    add files

--- support/jasmine.mjs
+++ support/jasmine.mjs
@@ -1,12 +1,12 @@
 export default {
   spec_dir: "./build",
   spec_files: [
-    'app/**/*.spec.ts',
-    'app/**/*spec.ts',
-    'app/**/*.ts',
-    'app/**/*.spec.js',
-    'app/**/*spec.js',
-    'app/**/*.js'
+    'app/**/*.spec.ts','app/**/**/*.spec.ts',
+    'app/**/*spec.ts',"app/**/**/*spec.ts",
+    'app/**/*.ts','app/**/**/*.ts',
+    'app/**/*.spec.js','app/**/**/*.spec.js',
+    'app/**/*spec.js','app/**/**/*.spec.js',
+    'app/**/*.js','app/**/**/*.js'
   ],
   helpers: [
     "helpers/*.?(m)js"
