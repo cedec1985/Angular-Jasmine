fb3c5d6d78a3e3546c543b19b79e3380bb7b2430
Cédric De Craim <cedricdecraim@msn.com>
2025-08-27 10:30:28 +0200

    delete build repository

--- src/build/app/reducers/counter-reducer/counter-reducer.js
+++
@@ -1,8 +0,0 @@
-import { createReducer, on } from '@ngrx/store';
-import { increment, decrement, reset } from '../../actions/counter-actions';
-export const initialState = 0;
-const reducer = createReducer(initialState, on(increment, (state) => state + 1), on(decrement, (state) => state - 1), on(reset, (_, action) => action.count));
-export function counterReducer(state, action) {
-    return reducer(state, action);
-}
-//# sourceMappingURL=counter-reducer.js.map
