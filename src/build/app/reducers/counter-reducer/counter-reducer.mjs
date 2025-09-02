663ce96b135ae568714a7834fdd877daa2432e90
Cédric De Craim <69531112+cedec1985@users.noreply.github.com>
2025-09-02 18:07:49 +0200

    Update counter-reducer.mjs

--- src/build/app/reducers/counter-reducer/counter-reducer.mjs
+++ src/build/app/reducers/counter-reducer/counter-reducer.mjs
@@ -1,5 +1,5 @@
 import { createReducer, on } from '@ngrx/store';
-import { increment, decrement, reset } from '../../actions/counter-actions.js';
+import { increment, decrement, reset } from '../../actions/counter-actions.mjs';
 export const initialState = 0;
 const reducer = createReducer(initialState, on(increment, (state) => state + 1), on(decrement, (state) => state - 1), on(reset, (_, action) => action.count));
 export function counterReducer(state, action) {
