fc1ebc52766a3603623f9c7585e92e4719981c75
Cédric De Craim <69531112+cedec1985@users.noreply.github.com>
2025-09-02 18:09:22 +0200

    Update counter-service.spec.mjs

--- src/build/app/services/counter-service.spec.mjs
+++ src/build/app/services/counter-service.spec.mjs
@@ -1,4 +1,4 @@
-import { CounterService } from "./counter-service.js";
+import { CounterService } from "./counter-service.mjs";
 describe('CounterService', () => {
     let service;
     beforeEach(() => {
