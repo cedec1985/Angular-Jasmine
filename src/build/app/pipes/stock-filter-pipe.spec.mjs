c0f1c2298d54940b7a7326d9e02c214ef8d85fab
Cédric De Craim <69531112+cedec1985@users.noreply.github.com>
2025-09-02 18:07:16 +0200

    Update stock-filter-pipe.spec.mjs

--- src/build/app/pipes/stock-filter-pipe.spec.mjs
+++ src/build/app/pipes/stock-filter-pipe.spec.mjs
@@ -1,4 +1,4 @@
-import { StockFilterPipe } from "./stock-filter-pipe.js";
+import { StockFilterPipe } from "./stock-filter-pipe.mjs";
 describe('StockFilterPipe', () => {
     let pipe;
     beforeEach(() => {
