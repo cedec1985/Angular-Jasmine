c756f41f622524dd9e9c3a4d5920125388a2e028
Cédric De Craim <69531112+cedec1985@users.noreply.github.com>
2025-09-08 13:06:06 +0200

    Add files via upload

--- src/app/components/service-counter-component3/service-counter-component3.ts
+++ src/app/components/service-counter-component3/service-counter-component3.ts
@@ -1,9 +1,11 @@
+import { CommonModule } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-service-counter-component3',
-  imports: [CommonModule,RouterOutlet,RouterLink,HttpClientModule],
+  imports: [CommonModule,HttpClientModule],
   templateUrl: './service-counter-component3.html',
   styleUrl: './service-counter-component3.css'
 })
