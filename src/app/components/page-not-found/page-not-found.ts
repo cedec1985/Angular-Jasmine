6abd9deb164830fced90306e7ae13a76e742bfa9
Cédric De Craim <69531112+cedec1985@users.noreply.github.com>
2025-09-08 13:05:40 +0200

    Add files via upload

--- src/app/components/page-not-found/page-not-found.ts
+++ src/app/components/page-not-found/page-not-found.ts
@@ -1,8 +1,10 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { RouterLink} from '@angular/router';
 
 @Component({
   selector: 'app-page-not-found',
-  imports: [CommonModule, RouterOutlet, RouterLink],
+  imports: [CommonModule, RouterLink],
   templateUrl: './page-not-found.html',
   styleUrl: './page-not-found.css'
 })
