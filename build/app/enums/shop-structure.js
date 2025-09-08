f176ce230245d2f9e1542fe468f2a0cf7911e21f
Cédric De Craim <69531112+cedec1985@users.noreply.github.com>
2025-09-08 15:47:04 +0200

    Add files via upload

---
+++ build/app/enums/shop-structure.js
@@ -0,0 +1,5 @@
+export var ShopStructure;
+(function (ShopStructure) {
+    ShopStructure[ShopStructure["StockLimited"] = 10] = "StockLimited"; // 10 produits en stock 
+})(ShopStructure || (ShopStructure = {}));
+//# sourceMappingURL=shop-structure.js.map
