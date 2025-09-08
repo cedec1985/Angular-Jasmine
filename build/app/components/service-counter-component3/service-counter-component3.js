ea6ac58a8a443fcce3947d59cf8d4035fd1ab1dd
Cédric De Craim <69531112+cedec1985@users.noreply.github.com>
2025-09-08 17:56:25 +0200

    Add files via upload

---
+++ build/app/components/service-counter-component3/service-counter-component3.js
@@ -0,0 +1,29 @@
+import { __decorate } from "tslib";
+import { CommonModule } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
+import { Component } from '@angular/core';
+let ServiceCounterComponent3 = class ServiceCounterComponent3 {
+    route;
+    id; // on initialise id à null ou à une chaîne de caractères
+    // id est une variable qui va contenir la valeur de : servcount3Id dans l'URL : /service-counter/servcount3Id
+    // on peut aussi utiliser un type plus spécifique comme number, mais ici on utilise String pour simplifier
+    // on importe ActivatedRoute pour pouvoir accéder aux paramètres de la route actuelle
+    constructor(route) {
+        this.route = route;
+    }
+    ngOnInit() {
+        this.route.paramMap.subscribe(params => {
+            this.id = params.get('servcount3Id') || ''; // récupère la valeur de servcount3Id dans l'URL
+        });
+    }
+};
+ServiceCounterComponent3 = __decorate([
+    Component({
+        selector: 'app-service-counter-component3',
+        imports: [CommonModule, HttpClientModule],
+        templateUrl: './service-counter-component3.html',
+        styleUrl: './service-counter-component3.css'
+    })
+], ServiceCounterComponent3);
+export { ServiceCounterComponent3 };
+//# sourceMappingURL=service-counter-component3.js.map
