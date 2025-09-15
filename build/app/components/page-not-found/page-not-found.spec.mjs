1111c7d67def29aada5733664627a0df5ccdb2ac
Cédric De Craim <69531112+cedec1985@users.noreply.github.com>
2025-09-15 14:20:21 +0200

    Add files via upload

---
+++ build/app/components/page-not-found/page-not-found.spec.mjs
@@ -0,0 +1,22 @@
+import { __awaiter } from "tslib";
+import { TestBed } from '@angular/core/testing';
+import { PageNotFound } from './page-not-found.mjs';
+describe('PageNotFound', () => {
+    let component;
+    let fixture;
+    beforeEach(() => __awaiter(void 0, void 0, void 0, function* () {
+        yield TestBed.configureTestingModule({
+            declarations: [PageNotFound]
+        }).compileComponents();
+        fixture = TestBed.createComponent(PageNotFound);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    }));
+    it('should create the component', () => {
+        expect(component).toBeTruthy();
+    });
+    it('should have the correct message', () => {
+        expect(component.message).toBe('Oups, page non trouvée !');
+    });
+});
+//# sourceMappingURL=page-not-found.spec.js.map
