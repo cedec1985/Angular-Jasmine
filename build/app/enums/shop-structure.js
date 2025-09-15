cc8b29829a1156dfb7e0d39ad7e64d40cc7bcd1c
Cédric De Craim <69531112+cedec1985@users.noreply.github.com>
2025-09-15 14:37:12 +0200

    Delete build/app/enums/shop-structure.js

--- build/app/enums/shop-structure.js
+++
@@ -1,5 +0,0 @@
-export var ShopStructure;
-(function (ShopStructure) {
-    ShopStructure[ShopStructure["StockLimited"] = 10] = "StockLimited"; // 10 produits en stock 
-})(ShopStructure || (ShopStructure = {}));
-//# sourceMappingURL=shop-structure.js.map
