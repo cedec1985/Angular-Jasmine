b1fc477eeb94de4d83dff5044bd31d1856bf4c0a
Cédric De Craim <69531112+cedec1985@users.noreply.github.com>
2025-09-15 14:29:52 +0200

    Add files via upload

---
+++ build/app/directives/attribute/attribut.directive.mjs
@@ -0,0 +1,35 @@
+import { __decorate } from "tslib";
+import { Directive, HostListener, Input } from '@angular/core';
+let AttributeDirective = class AttributeDirective {
+    constructor(el) {
+        this.el = el;
+        console.log(el);
+    }
+    // This input allows the directive to accept a color value
+    onMouseEnter() {
+        this.highlight(this.color || 'yellow');
+    }
+    onMouseLeave() {
+        this.highlight("");
+    }
+    highlight(color) {
+        this.el.nativeElement.style.backgroundColor = color;
+        //  on met à jour le background de l'élément sur lequel est appliquée la directive
+    }
+};
+__decorate([
+    Input('appAttribute')
+], AttributeDirective.prototype, "color", void 0);
+__decorate([
+    HostListener('mouseenter')
+], AttributeDirective.prototype, "onMouseEnter", null);
+__decorate([
+    HostListener('mouseleave')
+], AttributeDirective.prototype, "onMouseLeave", null);
+AttributeDirective = __decorate([
+    Directive({
+        selector: '[appAttribute]'
+    })
+], AttributeDirective);
+export { AttributeDirective };
+//# sourceMappingURL=attribut.directive.js.map
